7dfced47ee8c9afac24fe664319e7be6e949aaf2
Alex T. H <zelechos@hotmail.com>
2023-03-07 23:53:39 -0400

    v0.0.1

---
+++ spring-course/Introduction/src/main/java/com/pragmatic/spring/main/hello_world/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.pragmatic.spring.main.hello_world;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String send() {
+        log.info("execute the REST Controller HELLO WORLD");
+        return "<h1>Hello Hacker, Welcome to Spring<h1>";
+    }
+
+    @GetMapping("/sub")
+    public String subtitle() {
+        log.info("execute the subtitle of the code");
+        return "<hr/><h3>The project Spring is the Best<h3>";
+    }
+
+}
---
+++ spring-course/Introduction/src/test/java/com/pragmatic/spring/main/IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragmatic.spring.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
