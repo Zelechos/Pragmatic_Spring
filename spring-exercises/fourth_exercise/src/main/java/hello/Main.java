caa86a7ed2b2bc414fe827f71d8b751faa379a47
Alex T.H <zelechos@hotmail.com>
2023-03-31 22:13:42 -0400

    feat(spring_exercise): add fourth_exercise incomplete

---
+++ spring-exercises/fourth_exercise/src/main/java/hello/Main.java
@@ -0,0 +1,8 @@
+package main.java.hello;
+
+public class Main {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
