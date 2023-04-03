6b65e532f8712ecdc38899096bee8564022a967c
Alex T.H <zelechos@hotmail.com>
2023-04-03 17:16:48 -0400

    feat(spring-course): the introduction of MVC & Thymeleaf is finished

---
+++ spring-course/Thymeleaf/src/main/java/com/pragmatic/spring/main/sectors/Sector.java
@@ -0,0 +1,13 @@
+package com.pragmatic.spring.main.sectors;
+
+import lombok.Data;
+
+@Data
+public class Sector {
+
+    private String sectorNumber;
+    private String investigationArea;
+    private String antiquity;
+    private String primeReason;
+
+}
